7414ccb93ff152d0430f3d5d2614300a5f61bc34
Gabriel Guzman <gabe.r.guzman@gmail.com>
2025-12-08 23:57:13 -0600

    data

--- test_data.py
+++ test_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df=pd.read_parquet('data/1610612759_2023_rs.parquet')
+df=pd.read_csv('data/1610612759_2023_rs.csv')
 df.sort_values(by=['game_id','period', 'clock','actionNumber'],inplace=True)
 print(df.head(30))
 print(df.head(30)[['teamId']])
