cc319661b1ba35cf8d629b1295153cfd21132edc
Gabriel Guzman <gabe.r.guzman@gmail.com>
2025-12-08 23:09:49 -0600

    first data push

--- test_data.py
+++ test_data.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
 df=pd.read_parquet('data/1610612759_2023_rs.parquet')
+df.sort_values(by=['game_id','period', 'clock','actionNumber'],inplace=True)
+print(df.head(30))
+print(df.head(30)[['teamId']])
 print(df.columns)
