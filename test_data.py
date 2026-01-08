6a297e207978d8840ef1f010b887a4204aaa47b8
Gabriel Guzman <gabe.r.guzman@gmail.com>
2026-01-08 15:36:40 -0600

    update

--- test_data.py
+++ test_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df=pd.read_csv('data/1610612759_2023_rs.csv')
+df=pd.read_csv('data/ATL_2023_rs.csv')
 df.sort_values(by=['game_id','period', 'clock','actionNumber'],inplace=True)
-print(df.head(30))
+print(df[~df['blockPersonId'].isna()])
 print(df.head(30)[['teamId']])
 print(df.columns)
